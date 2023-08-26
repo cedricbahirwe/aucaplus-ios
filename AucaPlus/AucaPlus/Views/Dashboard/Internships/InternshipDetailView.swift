006bb753f41005d0d2f6e513c79ba48bab4e2a9e
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-08-26 12:00:41 +0200

    Add Link Preview sharing

--- AucaPlus/AucaPlus/Views/Dashboard/Internships/InternshipDetailView.swift
+++ AucaPlus/AucaPlus/Views/Dashboard/Internships/InternshipDetailView.swift
@@ -11,11 +11,36 @@ struct InternshipDetailView: View {
     let internship: Internship
     
     @EnvironmentObject private var bookmarkVM: BookmarkViewModel
+    @EnvironmentObject private var linksVM: LinksPreviewModel
+    
+    @State private var linkPreview: LinkPreview?
+    @State private var showShareSheet = false
+    
     var body: some View {
-        WebView(url: internship.link)
-            .task {
-                await bookmarkVM.view(internship: internship)
+        ZStack {
+            WebView(url: internship.link)
+            if showShareSheet, let metadata = linkPreview?.metadata {
+                ShareLinkView(metadata: metadata) {
+                    showShareSheet = false
+                }
+                .frame(width: 0, height: 0)
             }
+        }
+        .toolbar {
+            if linkPreview?.metadata != nil {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showShareSheet.toggle()
+                    } label: {
+                        Label("Share Link", systemImage: "square.and.arrow.up")
+                    }
+                }
+            }
+        }
+        .task {
+            await bookmarkVM.view(internship: internship)
+            linkPreview = await linksVM.getLinkPreview(for: internship.link)
+        }
     }
 }
 
@@ -24,6 +49,7 @@ struct InternshipDetailView_Previews: PreviewProvider {
     static var previews: some View {
         InternshipDetailView(internship: .example)
             .environmentObject(BookmarkViewModel())
+            .environmentObject(LinksPreviewModel())
     }
 }
 #endif
